1299f8537a2ecb3e99f2cbc9b277408728f7b194
John Kapantzakis <kapantzak@gmail.com>
2021-04-08 09:19:15 +0300

    Add 404 route

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from 'components/pages/Home';
 import About from 'components/pages/About';
 import Article from 'components/pages/Article';
+import NotFound404 from 'components/pages/NotFound404';
 
 export default (
   <BrowserRouter>
@@ -10,6 +11,7 @@ export default (
       <Route path="/" exact component={Home} />
       <Route path="/about" exact component={About} />
       <Route path="/article/:id" exact component={Article} />
+      <Route component={NotFound404} />
     </Switch>
   </BrowserRouter>
 );
